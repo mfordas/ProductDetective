90441ec6aebb5887e06a0b193a2aaf371d893d9c
Mateusz <mateusz.fordas@gmail.com>
2020-07-25 11:46:42 +0200

    Setup basics for app

---
+++ src/views/AppContent/AppContent.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+import '../../styling/main_styling.scss'
+
+
+const AppContent = () => {
+    return (
+        <div className="AppContainer">
+            <div>Setup for allergen app</div>
+            </div>
+    );
+}
+
+export default AppContent;
