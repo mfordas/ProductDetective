6af2ef59b75e6c0e7a8364d054288fe1c6547332
Mateusz <mateusz.fordas@gmail.com>
2020-08-02 16:30:18 +0200

    Changed barcode refresh

--- src/components/Product_scanner/productScanner.jsx
+++ src/components/Product_scanner/productScanner.jsx
@@ -2,37 +2,47 @@ import React, { useState, useEffect } from 'react';
 import { BrowserBarcodeReader } from '@zxing/library';
 
 import '../../styling/main_styling.scss';
-import imgSrc from './pobrane.png';
 
 const codeReader = new BrowserBarcodeReader();
 
 const ProductScannerContent = () => {
     const [barcode, setBarcode] = useState('123456789');
     const [inputDevicesList, setDevices] = useState([]);
+    const [error, setError] = useState('error');
+    const [counter, setCount] = useState(0);
 
     useEffect(() => {
-        const listOfInputDevices = codeReader
+       const collectingListOfDevices = async () => {
+        const listOfInputDevices = await codeReader
             .listVideoInputDevices()
             .then(videoInputDevices => {
                 videoInputDevices.forEach(device =>
                     console.log(`${device.label}, ${device.deviceId}`)
                 );
             })
-            .catch(err => console.error(err));
+            .catch(err => setError(`${err}`));
 
-    }, [])
+            return inputDevicesList;
+        }
+    }, [inputDevicesList]);
 
     useEffect(() => {
         codeReader
             .decodeOnceFromVideoDevice(undefined, 'video')
-            .then(result => setBarcode(result.text))
-            .catch(err => console.error(err));
-    })
+            .then(result => {
+                setBarcode(result.text);
+                setCount(counter++)
+            })
+            .catch(err => setError(`${err}`));
+    }, [barcode, counter]);
 
     return (
         <div className='mainWindow'>
             <video id="video" width="300" height="200"></video>
             {barcode}
+            {inputDevicesList}
+            <div>{error}</div>
+            {counter}
         </div>
     );
 }
