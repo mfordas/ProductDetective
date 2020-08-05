f939b42720e3bdc7b65c786620981da92948dced
Mateusz <mateusz.fordas@gmail.com>
2020-08-05 20:46:15 +0200

    Simple main page created

---
+++ src/components/Main_page/mainPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MainPageContent = () => {
+    return (
+        <div className='mainPage'>
+            <p>
+                Welcome on my simple page for checking Nutrinion score of products.
+                Page is based on <a href='https://world.openfoodfacts.org/' target='_blank'><b>Open Food Facts API</b></a>
+            </p>
+            <p>
+                If you want to check some product just go to Product Scanner tab, allow your website to use camera and scan barcode of product! It's so simple.
+            </p>
+            <p>
+                Best regards, I hope you'll enjoy my app.
+            </p>
+        </div>
+    )
+};
+
+export default MainPageContent;
